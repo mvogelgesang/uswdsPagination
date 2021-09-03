dde1639d59245b8cefcec26ba8e04da48fae444a
mvogelgesang <movogelgesang@gmail.com>
2021-09-03 10:50:21 -0400

    removed old logic, added form to let users test new scenarios themselves

--- paginationTest.js
+++ paginationTest.js
@@ -1,7 +1,11 @@
-// var totalPages = prompt("how many total pages?");
-// var currentPage = prompt("what current page are you on?");
-// console.log("total pages: " + totalPages);
-// console.log("current page: " + currentPage);
+function testPagination() {
+  console.log("fire!");
+  var totalPages = document.getElementById("totalPages").value;
+  var currentPage = document.getElementById("currentPage").value;
+  var testPagination = new Pagination(totalPages, currentPage);
+  testPagination.buildPagination();
+  document.getElementById("testResult").innerHTML = testPagination.toString();
+}
 
 class Pagination {
   constructor(totalPages, currentPage) {
@@ -18,87 +22,7 @@ class Pagination {
     );
   }
 
-  buildPagination(totalPages, currentPage) {
-    // start with the current page
-    this.paginationArray.push(createPage(currentPage, true));
-
-    // handle middle conent
-    //work left of the currentPage
-    if (currentPage - 1 >= 2 && totalPages <= 7) {
-      for (var i = currentPage - 1; i >= 1; i--) {
-        this.paginationArray.unshift(createPage(i, false));
-      }
-    } else if (totalPages - currentPage < 1 && currentPage >= 7) {
-      for (var i = currentPage - 1; i >= currentPage - 4; i--) {
-        this.paginationArray.unshift(createPage(i, false));
-      }
-      this.paginationArray.unshift(createPage(this.overflowIndicator, false));
-      this.paginationArray.unshift(createPage(1, false));
-    } else if (totalPages - currentPage < 2 && currentPage >= 7) {
-      for (var i = currentPage - 1; i >= currentPage - 3; i--) {
-        this.paginationArray.unshift(createPage(i, false));
-      }
-      this.paginationArray.unshift(createPage(this.overflowIndicator, false));
-      this.paginationArray.unshift(createPage(1, false));
-    } else if (totalPages - currentPage < 3 && currentPage >= 6) {
-      for (var i = currentPage - 1; i >= currentPage - 2; i--) {
-        this.paginationArray.unshift(createPage(i, false));
-      }
-      this.paginationArray.unshift(createPage(this.overflowIndicator, false));
-      this.paginationArray.unshift(createPage(1, false));
-    } else if (currentPage - 1 > 3) {
-      this.paginationArray.unshift(createPage(currentPage - 1, false));
-      this.paginationArray.unshift(createPage(this.overflowIndicator, false));
-      this.paginationArray.unshift(createPage(1, false));
-    } else if (currentPage - 1 == 3) {
-      this.paginationArray.unshift(createPage(currentPage - 1, false));
-      this.paginationArray.unshift(createPage(currentPage - 2, false));
-      this.paginationArray.unshift(createPage(currentPage - 3, false));
-    } else if (currentPage - 1 >= 2) {
-      this.paginationArray.unshift(createPage(currentPage - 1, false));
-      this.paginationArray.unshift(createPage(currentPage - 2, false));
-    } else if (currentPage - 1 >= 1) {
-      this.paginationArray.unshift(createPage(currentPage - 1, false));
-    } else {
-    }
-    // work right of the currentPage
-    if (totalPages - currentPage > 3 && totalPages <= 7) {
-      for (var i = currentPage + 1; i < totalPages; i++) {
-        this.paginationArray.push(createPage(i, false));
-      }
-    } else if (totalPages - currentPage > 3 && currentPage <= 3) {
-      for (var i = currentPage + 1; i < 6; i++) {
-        this.paginationArray.push(createPage(i, false));
-      }
-      this.paginationArray.push(createPage(this.overflowIndicator, false));
-    } else if (totalPages - currentPage > 3) {
-      this.paginationArray.push(createPage(currentPage + 1, false));
-      this.paginationArray.push(createPage(this.overflowIndicator, false));
-    } else if (totalPages - currentPage == 3) {
-      this.paginationArray.push(createPage(currentPage + 1, false));
-      this.paginationArray.push(createPage(currentPage + 2, false));
-      //this.paginationArray.push(createPage(currentPage + 3, false));
-    } else if (totalPages - currentPage >= 2) {
-      this.paginationArray.push(createPage(currentPage + 1, false));
-      this.paginationArray.push(createPage(currentPage + 2, false));
-    } else if (totalPages - currentPage > 1) {
-      this.paginationArray.push(createPage(currentPage + 1, false));
-    } else {
-    }
-    // handle last number
-    if (totalPages != currentPage && totalPages - currentPage != 2) {
-      this.paginationArray.push(createPage(totalPages, false));
-    }
-
-    // tack on Previous & Next buttons
-    if (currentPage != 1) {
-      this.paginationArray.unshift(createPage("Previous", false));
-    }
-    if (currentPage != totalPages) {
-      this.paginationArray.push(createPage("Next", false));
-    }
-  }
-  buildPaginationRefactor() {
+  buildPagination() {
     this.calculateNoSlots();
     this.createSlots();
     this.fillSlots();
@@ -331,32 +255,23 @@ var scenarios = [
 
 /// RUN TESTS
 for (var j = 0; j < scenarios.length; j++) {
-  var pager1 = new Pagination(
-    scenarios[j].totalPages,
-    scenarios[j].currentPage
-  );
-  pager1.buildPagination(scenarios[j].totalPages, scenarios[j].currentPage);
   var pagerRef = new Pagination(
     scenarios[j].totalPages,
     scenarios[j].currentPage
   );
-  pagerRef.buildPaginationRefactor();
+  pagerRef.buildPagination();
   var table = document.getElementById("tbody");
   // Create an empty <tr> element and add it to the 1st position of the table:
   let row = table.insertRow(-1);
 
   // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
   row.insertCell(0).innerHTML =
-    pager1.toString() == scenarios[j].expected
-      ? '<span style="color: green"><i class="fas fa-check-circle"></i></span>'
-      : '<span style="color: red"><i class="fas fa-times-circle"></i></span>';
-  row.insertCell(1).innerHTML =
     pagerRef.toString() == scenarios[j].expected
       ? '<span style="color: green"><i class="fas fa-check-circle"></i></span>'
       : '<span style="color: red"><i class="fas fa-times-circle"></i></span>';
-  row.insertCell(2).innerHTML = scenarios[j].currentPage;
-  row.insertCell(3).innerHTML = scenarios[j].totalPages;
-  row.insertCell(4).innerHTML = scenarios[j].explanation;
-  row.insertCell(5).innerHTML = scenarios[j].expected;
-  row.insertCell(6).innerHTML = pagerRef.toString();
+  row.insertCell(1).innerHTML = scenarios[j].currentPage;
+  row.insertCell(2).innerHTML = scenarios[j].totalPages;
+  row.insertCell(3).innerHTML = scenarios[j].explanation;
+  row.insertCell(4).innerHTML = scenarios[j].expected;
+  row.insertCell(5).innerHTML = pagerRef.toString();
 }
